3b641e99c5c2c0ffd125c1709c54249f11a045e4
shivitri <shivitripathi684@gmail.com>
2026-06-14 20:19:15 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec gunicorn \
-    --bind 0.0.0.0:10000 \
-    --workers 1 \
-    --timeout 120 \
-    --worker-class sync \
-    --access-logfile - \
-    --error-logfile - \
-    main:app
